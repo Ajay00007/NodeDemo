74521099097d8121399b963b610419014ff45f0b
Ajay00007 <77910334+Ajay00007@users.noreply.github.com>
2023-08-02 11:28:22 +0530

    Update server.js

--- Node/server.js
+++ Node/server.js
@@ -1,15 +1,9 @@
 const express = require("express");
-
 const cors = require("cors");
-
 const mysql = require("mysql");
-
 const app = express();
-
 app.use(express.json());
-
 app.use(cors());
-
 const db = mysql.createConnection({ host: "localhost",
                                     user: "root",
                                     password: "ajay007@#",
@@ -35,89 +29,40 @@ app.get("/login/:email/:password", (req, res) => {
          if (err) throw err;
          return res.json(data);
      });
-
  });
 
 app.get("/emplist", (req, res) => {
    const sql = "SELECT * FROM emp";
     db.query(sql, (err, data) => {
         if (err) throw err;
-
         return res.json(data);
-
     });
-
 });
 
 app.get("/emp/:id", (req, res) => {
-
     const id = req.params.id;
-
     const sql = "SELECT * FROM emp where ID= ? ";
-
     db.query(sql, [id], (err, results) => {
-
         if (err)  throw err;
-
         return res.json(results);
-
     })
-
 });
 
-
-
-
 app.post("/addemp", (req, res) => {
-
-
-
-
     const sql = "INSERT INTO emp (`name`, `sex`, `dob`, `salary`, `department` ) VALUES (?)";
-
     const values = [
-
-
-
-
        req.body.name,
-
-
-
-
        req.body.sex,
-
-
-
-
        req.body.dob,
-
-       
-
        req.body.salary,
-
-
-
-
        req.body.department,
-
     ]
 
     db.query(sql, [values], (err, data) => {
-
         if (err)  throw err;
-
         return res.json(data);
-
     })
-
-
-
-
 })
-
-
-
 
 app.put("/user/edit/:id", (req, res) => {
     const sql = "update emp set name = ?, sex = ?, dob = ?, salary = ?, department = ?  WHERE ID = ?";
@@ -128,9 +73,6 @@ app.put("/user/edit/:id", (req, res) => {
        return res.json(data);
     })
 })
-
-
-
 
 app.delete("/delemp/:id", (req, res) => {
     const sql = "DELETE FROM emp WHERE ID = ?";
